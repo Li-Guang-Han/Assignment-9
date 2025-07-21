c9cf69e8a135a11b84538a2d3241dac257c90363
Garrett Li <166572446+Li-Guang-Han@users.noreply.github.com>
2025-07-20 23:17:02 -0500

    Add files via upload

---
+++ Assignment 9/Assignment 9/Assignment_9App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_9App.swift
+//  Assignment 9
+//
+//  Created by Garrett Li on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment 9/Assignment 9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment 9
+//
+//  Created by Garrett Li on 7/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
